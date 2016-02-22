feb0de2d21902f8fc0db2b4eb1d9528aac523784
Nicolas Sorin <nicolas.sorin@insa-lyon.fr>
2016-02-22 16:54:19 +0100

    HR: Ajout objet Team et ses services, début d'interface

---
+++ src/modules/hr/services/services.js
@@ -0,0 +1,91 @@
+// ----------------------- TEAM INTERFACE SERVICES CLASS ----------------------------------
+
+var TeamServicesInterface = new function() {
+
+  this.meta = {
+    // --- (object)
+    OBJECT: 'team',
+    // --- (actions)
+    ACTION: {
+      GET_TEAM_BY_ID:'byidt',
+      GET_TEAM_BY_DIV:'bystt',
+      GET_TEAM_MEMBERS:'memt',
+      GET_ALL_TEAMS:'allt',
+      GET_USER_TEAMS:'allut',
+      GET_ALL_DIVISIONS:'alldiv',
+      INSERT_MEMBER:'insmem',
+      DELETE_MEMBER:'delmem',
+      INSERT:'insert',
+      UPDATE:'update',
+      DELETE:'delete'
+    }
+  };
+
+  this.getAllTeams = function(successHandler) {
+   return DoleticServicesInterface.callService(this.meta.OBJECT, this.meta.ACTION.GET_ALL_TEAMS, {}, successHandler); 
+  }
+  this.getAllDivisions = function(successHandler) {
+   return DoleticServicesInterface.callService(this.meta.OBJECT, this.meta.ACTION.GET_ALL_DIVISIONS, {}, successHandler); 
+  }
+  this.getUserTeams = function(successHandler) {
+   return DoleticServicesInterface.callService(this.meta.OBJECT, this.meta.ACTION.GET_USER_TEAMS, {}, successHandler); 
+  }
+  this.getTeamMembers = function(statusId, successHandler) {
+   return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.GET_TEAM_MEMBERS, 
+            { id:id }, 
+            successHandler); 
+  }
+  this.getById = function(id, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.GET_TEAM_BY_ID, 
+            { id: id }, 
+            successHandler); 
+  }
+  this.insertMember = function(id, memberId, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.INSERT_MEMBER, 
+            { 
+              id: id,
+              memberId: memberId
+            }, 
+            successHandler); 
+  }
+  this.deleteMember = function(id, memberId, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.DELETE_MEMBER, 
+            { 
+              id: id,
+              memberId: memberId
+            }, 
+            successHandler); 
+  }
+  this.insert = function(name, leaderId, division, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.INSERT, 
+            {
+              name:name,
+              leaderId:leaderId,
+              division:division
+            }, 
+            successHandler); 
+  }
+  this.update = function(id, name, leaderId, division, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.UPDATE, 
+            {
+              id:id,
+              name:name,
+              leaderId:leaderId,
+              division:division
+            }, 
+            successHandler);
+  }
+  this.delete = function(id, successHandler) {
+    return DoleticServicesInterface.callService(
+            this.meta.OBJECT, this.meta.ACTION.DELETE, 
+            { id: id }, 
+            successHandler); 
+  } 
+
+}
---
+++ src/modules/hr/ui/admin.js
@@ -0,0 +1,139 @@
+var DoleticUIModule = new function() {
+	/**
+	 *	Parent abstract module
+	 */
+	this.super = new AbstractDoleticUIModule('Support_UIModule', 'Paul Dautry', '1.0dev');
+	/**
+	 *	Override render function
+	 */
+	this.render = function(htmlNode) {
+		this.super.render(htmlNode, this);
+		// refresh lists
+		this.refreshTicketLists();
+	}
+	/**
+	 *	Override build function
+	 */
+	this.build = function() {
+		return "<div class=\"ui two column grid container\"> \
+				  <div class=\"row\"> \
+				  </div> \
+				  <div class=\"row\"> \
+				  <div class=\"three wide column\"> \
+					</div> \
+					<div class=\"ten wide column\"> \
+						<div class=\"ui horizontal divider\"><i class=\"red radio icon\"></i> Tickets ouverts</div> \
+						<div id=\"open_ticket_list\" class=\"ui very relaxed celled selection list\"> \
+						  <!-- OPEN TICKETS WILL GO HERE --> \
+						</div> \
+					</div> \
+					<div class=\"three wide column\"> \
+					</div> \
+				  </div> \
+				  <div class=\"row\"> \
+				  </div> \
+				  <div class=\"row\"> \
+				  <div class=\"three wide column\"> \
+					</div> \
+					<div class=\"ten wide column\"> \
+						<div class=\"ui horizontal divider\"><i class=\"orange spinner icon\"></i> Tickets en traitement</div> \
+						<div id=\"wip_ticket_list\" class=\"ui very relaxed celled selection list\"> \
+						  <!-- WORK IN PROGRESS TICKETS WILL GO HERE --> \
+						</div> \
+					</div> \
+					<div class=\"three wide column\"> \
+					</div> \
+				  </div> \
+				</div>";
+	}
+	/**
+	 *	Override uploadSuccessHandler
+	 */
+	this.uploadSuccessHandler = function(id, data) {
+		this.super.uploadSuccessHandler(id, data);
+	}
+
+	this.nightMode = function(on) {
+	    if(on) {
+	    	$('.ui.button.treat').attr('class', 'ui button treat inverted');
+	    	$('.ui.horizontal.divider').attr('class', 'ui horizontal divider inverted');
+	    	$('#open_ticket_list').attr('class', 'ui very relaxed celled selection list inverted');
+	    	$('#wip_ticket_list').attr('class', 'ui very relaxed celled selection list inverted');
+	    } else {
+	    	$('.ui.button.treat.inverted').attr('class', 'ui button treat');
+	    	$('.ui.horizontal.divider.inverted').attr('class', 'ui horizontal divider');
+	    	$('#open_ticket_list').attr('class', 'ui very relaxed celled selection list');
+	    	$('#wip_ticket_list').attr('class', 'ui very relaxed celled selection list');
+	    }
+  	}
+
+// ---- OTHER FUNCTION REQUIRED BY THE MODULE ITSELF
+
+	this.fillList = function(list, action, json) {
+		// create content var to build html
+		var content = "";
+		// iterate over values to build options
+		for (var i = 0; i < json.length; i++) {
+			content += "<div class=\"item\"> \
+							<div class=\"right floated content\"> \
+      							<div class=\"ui button treat\" onClick=\"DoleticUIModule.treatTicket('"+json[i].id+"');\">"+action+"</div> \
+    						</div> \
+						  <div class=\"middle aligned content\"> \
+						    <a class=\"header\">"+json[i].subject+"</a> \
+						    <div class=\"description\">"+json[i].data+"</div>  \
+						  </div> \
+						</div>";
+		};
+		// insert html content
+		$(list).html(content);
+		// update night mode
+		DoleticUISettings.applySettings();
+	}
+	/**
+	 *	Passe le ticket dans l'etat "en traitement"
+	 */
+	this.treatTicket = function(id) {
+		TicketServicesInterface.nextTicketStatus(id, DoleticUIModule.refreshTicketLists);
+	}
+	/**
+	 *	Refresh lists after ticket status change
+	 */
+	this.refreshTicketLists = function() {
+		// fill open ticket list
+		DoleticUIModule.fillOpenTicketsList();
+		// fill wip ticket list
+		DoleticUIModule.fillWorkInProgressTicketsList();
+	}
+
+	this.fillOpenTicketsList = function() {
+		// clear list first
+		$('#open_ticket_list').html('');
+		// retrieve tickets
+		TicketServicesInterface.getTicketsByStatus(1,function(data) {
+			// if no service error
+			if(data.code == 0 && data.object != "[]") {
+				// fill list
+				DoleticUIModule.fillList('#open_ticket_list', 'Traiter', data.object);
+			} else {
+				// use default service service error handler
+				DoleticServicesInterface.handleServiceError(data);
+			}
+		});
+	}
+	this.fillWorkInProgressTicketsList = function() {
+		// clear list first
+		$('#wip_ticket_list').html('');
+		// retrieve tickets
+		TicketServicesInterface.getTicketsByStatus(2,function(data) {
+			// if no service error
+			if(data.code == 0 && data.object != "[]") {
+				// fill list
+				DoleticUIModule.fillList('#wip_ticket_list', 'Résolu !', data.object);
+			} else {
+				// use default service service error handler
+				DoleticServicesInterface.handleServiceError(data);
+			}
+		});
+	}
+
+}
